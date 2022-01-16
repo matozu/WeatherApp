f3504e067f4a7ef1ac80403f5f8712a4fc31a48a
matozu <mato.zunac@gmail.com>
2022-01-16 10:27:52 +0100

    clean start with sass

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Weather App</h1>
+</template>
+
+<script setup>
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style lang="scss">
+$color: red;
+
+h1 {
+  color: $color;
+}
+</style>
